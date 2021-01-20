794109c9ceedd63884ff528c639ea3c5b4947c30
takaibun <deepbluehw@163.com>
2021-01-21 02:03:02 +0800

    初始化仓库，实现了第一个简单的算法[数组中三数之积的最大值]

---
+++ src/simple/MaximumProduct.java
@@ -0,0 +1,14 @@
+package simple;
+
+import java.util.Arrays;
+
+/**
+ * @author takaibun
+ */
+public class MaximumProduct {
+    public int maximumProduct(int[] nums){
+        Arrays.sort(nums);
+        return Math.max(nums[nums.length-1]*nums[nums.length-2]*nums[nums.length-3],nums[0]*nums[1]*nums[nums.length-1] );
+    }
+
+}
