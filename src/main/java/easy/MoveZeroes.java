677906ae4e94aa8ff39dd3c1b0a14ed20d9ca247
takaibun <deepbluehw@163.com>
2021-01-27 11:50:59 +0800

    合并作业中学习过的算法

---
+++ src/main/java/easy/MoveZeroes.java
@@ -0,0 +1,21 @@
+package Week_01;
+
+/**
+ * @author takaibun
+ */
+public class MoveZeroes {
+
+    public void moveZeroes(int[] nums) {
+        int idx=0;
+        for (int i = 0;i < nums.length;i++ ){
+            if(nums[i]==0){
+                idx++;
+            }else{
+                nums[i-idx]=nums[i];
+            }
+        }
+        for(;idx>0;idx--){
+            nums[nums.length-idx]=0;
+        }
+    }
+}
